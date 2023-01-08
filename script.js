739a9386894ba050d78cfd1305a4b5bdf526453f
YeHtutAung <yehaung.nov@gmail.com>
2023-01-08 18:02:41 +0100

    game update done

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ let Audio_cross = new Audio('audio/beep2.mp3');
 let Audio_circle = new Audio('audio/shortTone3.mp3');
 let Audio_win = new Audio('audio/win.mp3');
 let Audio_restart = new Audio('audio/newgame.mp3');
+let Audio_lose = new Audio('audio/loose.mp3');
 
 
 function fillShape(id) {
-    if (!fields[id] && !gameOver)  //Wenn das Feld gefüllt ist und Wenn wir nicht gameOver haben
+    if (!fields[id] && !gameOver)  //Wenn das Feld nicht gefüllt ist und Wenn wir nicht gameOver haben
     {
         if (currentShape == 'cross') {
             currentShape = 'circle';
@@ -28,6 +29,7 @@ function fillShape(id) {
         draw();
         checkForWin();
     }
+
 }
 
 
@@ -35,12 +37,11 @@ function draw() {
     for (let i = 0; i < fields.length; i++) {
         if (fields[i] == 'circle') {
             document.getElementById(`circle-${i}`).classList.remove('d-none');
-            
+
         }
 
         if (fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
-            
         }
     }
 }
@@ -71,7 +72,7 @@ function checkForWin() {
 
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
-        document.getElementById('line-4').style = 'top: 404px';
+        document.getElementById('line-4').style = 'top: 198px';
         document.getElementById('line-4').style.transform = 'translateX(-138px) rotate(90deg) scaleX(1)';
         document.getElementById('line-4').classList.remove('d-none');
     }
@@ -84,7 +85,7 @@ function checkForWin() {
 
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
-        document.getElementById('line-6').style = 'top: 404px';
+        document.getElementById('line-6').style = 'top: 198px';
         document.getElementById('line-6').style.transform = 'translateX(138px) rotate(90deg) scaleX(1)';
         document.getElementById('line-6').classList.remove('d-none');
     }
@@ -105,17 +106,48 @@ function checkForWin() {
         console.log('gewonnen', winner);
         gameOver = true; //Beim Gewinnen Gameover=true
         console.log(gameOver);
-        setTimeout(function(){
+        //countArrayIndex = 0;
+        setTimeout(function () {
             Audio_win.play();
         }, 500);
 
         setTimeout(function () {
-            document.getElementById('gameOver').classList.remove('d-none'); //GameOver Img Anzeige wird um 3 Sek verzögert aufgerufen
+            document.getElementById('gameOver').classList.remove('d-none'); //GameOver Img Anzeige wird um 1,2 Sek verzögert aufgerufen
             document.getElementById('restart-btn').classList.remove('d-none');
             document.getElementById('showWinner').classList.remove('d-none');
-            document.getElementById('showWinner').innerHTML='';
-            document.getElementById('showWinner').innerHTML += 'This round, ' + winner + ' has won. good Job!';
-        }, 1200); //nach 3Sek aufgerufen
+            document.getElementById('showWinner').innerHTML = '';
+            if (winner == 'cross') {
+                document.getElementById('showWinner').innerHTML += 'The Winner is Player2/' + winner + ' -- good Game!';
+            } else {
+                document.getElementById('showWinner').innerHTML += 'The Winner is Player1/' + winner + ' -- good Game!';
+            }
+        }, 1200); //Zeit verzögert aufgerufen
+    }
+
+    //Fall für Remi Wenn Keiner gewonnen hat
+    if ((fields[0] && fields[1] && fields[2] && fields[3] && fields[4] && fields[5]
+        && fields[6] && fields[7] && fields[8] &&fields[9] != '') && !gameOver) 
+    {   
+        gameOver = true; //Beim Remi auch Gameover=true
+        console.log(gameOver);
+        Audio_lose.play();
+        setTimeout(function(){ //Lininen und die X-O löschen
+            for (let i = 1; i < 9; i++) {
+                document.getElementById('line-' + i).classList.add('d-none');
+            }
+        
+            for (let i = 0; i < 9; i++) {
+                document.getElementById('circle-' + i).classList.add('d-none');
+                document.getElementById('cross-' + i).classList.add('d-none');
+            }
+        },1500)
+        setTimeout(function(){
+            document.getElementById('restart-btn').classList.remove('d-none');
+            document.getElementById('noWinner').classList.remove('d-none');
+            document.getElementById('showWinner').classList.remove('d-none');
+            document.getElementById('showWinner').innerHTML = '';
+            document.getElementById('showWinner').innerHTML = 'No Player is winning';
+        },500)
     }
 
 }
@@ -135,17 +167,6 @@ function restart() {
         document.getElementById('circle-' + i).classList.add('d-none');
         document.getElementById('cross-' + i).classList.add('d-none');
     }
+    document.getElementById('noWinner').classList.add('d-none');
 }
 
-
-function startSlider(){
-    setTimeout(function(){
-        document.getElementById('img-1').style = 'transform: translateX(-100%)';
-        document.getElementById('img-2').style = 'transform: translateX(0)';
-    },5000)
-    setTimeout(function(){
-        document.getElementById('img-1').style = 'transform: translateX(0)';
-        document.getElementById('img-2').style = 'transform: translateX(100%)';
-        startSlider();
-    },10000)
-}
